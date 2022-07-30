4d434757a8b37f56ff0cc8979e2955bbc4864e60
damil27 <dhamilare27@gmail.com>
2022-07-30 11:07:34 +0100

    disable axios

--- src/context/gihhub/GithubActions.js
+++ src/context/gihhub/GithubActions.js
@@ -1,9 +1,5 @@
-import axios from "axios";
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
-const github = axios.create({
-  baseURL: GITHUB_URL,
-});
 export const searchUsers = async (text) => {
   const params = new URLSearchParams({
     q: text,
